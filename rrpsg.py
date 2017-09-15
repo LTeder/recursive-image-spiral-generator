7b3e4beb8ec22ee05772fb5d52fc95e29df6d76d
EvilMossman <lteder01@gmail.com>
2017-09-15 12:31:35 -0400

    Migrated entire program into a Class, added comments to explain initialization.

--- rrpsg.py
+++ rrpsg.py
@@ -1,59 +1,69 @@
-from Tkinter import *
+import Tkinter as tk
 from math import pi, cos, sin
 
-root = Tk()
-root.title('Recursive Regular Polygon Spiral Generator')
 
-vertex_number = 5
-recursion_ratio = 0.75 # Part of master segments where new points (for new polygons) shall be drawn
+class Rrpsg(tk.Frame):
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
+        self.vertex_number = 5
+        self.recursion_ratio = 0.75 # Part of master segments where new points (for new polygons) shall be drawn
 
-def draw():
-    c.delete("all")
-    vertex_number = int(e1.get())
-    recursion_ratio = float(e2.get())
-    verticies = []
+        # Establishes drawing canvas at the top of the window with default dimensions
+        self.c = tk.Canvas(root, height=500, width=501)
+        self.c.pack(anchor=tk.N)
 
-    for vertex in range(vertex_number):
-        verticies.append((
-            250*cos((vertex*2*pi)/vertex_number)+252,
-            250*sin((vertex*2*pi)/vertex_number)+250))
-    verticies.append((502,250))
-    # Slight deviations in dimensions every once in a while, to ensure the drawing fits well
-    
-    for iteration in range(500):  
-        for coords in verticies[:-1]:
-            c.create_line(coords, verticies[verticies.index(coords)+1])
-        for coords in verticies[:-1]:
-            verticies[verticies.index(coords)] = (
-                coords[0]-(recursion_ratio*(coords[0]-verticies[verticies.index(coords)+1][0])),
-                coords[1]-(recursion_ratio*(coords[1]-verticies[verticies.index(coords)+1][1])))
-        verticies[len(verticies)-1] = verticies[0]
+        # Labels packed in entries, entries packed in options
+        self.options = tk.Frame(root)
+        self.options.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+        self.entries = tk.Frame(self.options)
+        self.entries.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.labels = tk.Frame(self.entries)
+        self.labels.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-c = Canvas(root, height=500, width=501)
-c.pack(anchor=N)
+        # Individual entries and labels packed to the top and bottom of their respective frames
+        self.l1 = tk.Label(self.labels, text="Number of Verticies:")
+        self.l1.pack(side=tk.TOP, fill=tk.X, expand=1, padx=10, pady=10)
+        self.e1 = tk.Entry(self.entries, width=20, fg="black")
+        self.e1.pack(side=tk.TOP, fill=tk.X, expand=1, padx=10, pady=10)
+        self.e1.insert(0, "5")
 
-options = Frame(root)
-options.pack(side=RIGHT, fill=BOTH, expand=1)
-entries = Frame(options)
-entries.pack(side=LEFT, fill=BOTH, expand=1)
-labels = Frame(entries)
-labels.pack(side=LEFT, fill=BOTH, expand=1)
+        self.l2 = tk.Label(self.labels, text="Recursion Ratio:")
+        self.l2.pack(side=tk.BOTTOM, fill=tk.X, expand=1, padx=10, pady=10)
+        self.e2 = tk.Entry(self.entries, width=20, fg="black")
+        self.e2.pack(side=tk.BOTTOM, fill=tk.X, expand=1, padx=10, pady=10)
+        self.e2.insert(0, "0.75")
 
-e1 = Entry(entries, width=20, fg="black")
-e1.pack(side=TOP, fill=X, expand=1, padx=10, pady=10)
-e1.insert(0, "5")
-e2 = Entry(entries, width=20, fg="black")
-e2.pack(side=BOTTOM, fill=X, expand=1, padx=10, pady=10)
-e2.insert(0, "0.75")
+        # Button packed in options, outside of entries/labels frames
+        self.b = tk.Button(self.options, text="RENDER", height=2, width=15, command=self.draw)
+        self.b.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1, padx=10, pady=10)
 
-l1 = Label(labels, text="Number of Verticies:")
-l1.pack(side=TOP, fill=X, expand=1, padx=10, pady=10)
-l2 = Label(labels, text="Recursion Ratio:")
-l2.pack(side=TOP, fill=X, expand=1, padx=10, pady=10)
+        self.draw() # Draws default image
 
+    def draw(self):
+        self.c.delete("all")
+        self.vertex_number = int(self.e1.get())
+        self.recursion_ratio = float(self.e2.get())
+        verticies = []
 
-b = Button(options, text="RENDER", height=2, width=15, command=draw)
-b.pack(side=RIGHT, fill=BOTH, expand=1, padx=10, pady=10)
+        for vertex in range(self.vertex_number):
+            verticies.append((
+                250*cos( (vertex*2*pi)/self.vertex_number) +252,
+                250*sin( (vertex*2*pi)/self.vertex_number) +250))
+        verticies.append((502,250))
+        # Slight deviations in dimensions every once in a while, to ensure the drawing fits well
+        
+        for iteration in range(500):  
+            for coords in verticies[:-1]:
+                self.c.create_line(coords, verticies[verticies.index(coords)+1])
+            for coords in verticies[:-1]:
+                verticies[verticies.index(coords)] = (
+                    coords[0]-(self.recursion_ratio*(coords[0]-verticies[verticies.index(coords)+1][0])),
+                    coords[1]-(self.recursion_ratio*(coords[1]-verticies[verticies.index(coords)+1][1])))
+            verticies[len(verticies)-1] = verticies[0]
+          
 
-draw()
-mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Recursive Regular Polygon Spiral Generator")
+    Rrpsg(root).pack()
+    root.mainloop()
